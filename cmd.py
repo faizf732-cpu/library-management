7fda0fad003675841304065b94857bc8d4f06c79
faizf732-cpu <mohammedfaiz112006@gmail.com>
2026-04-16 13:06:43 +0530

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -189,7 +189,7 @@ git push -u origin main
 
 
 
-Pipelines:
+Pipelines Jenkins:
 
 pipeline {
     agent any
@@ -221,5 +221,46 @@ pipeline {
             }
         }
 
+    }
+}
+
+
+
+Pipeline maven:
+
+pipeline {
+    agent any
+    environment {
+        DOCKER_IMAGE = 'faiz732/html_demo'
+    }
+    stages {
+        stage('Clone Code') {
+            steps {
+                git branch: 'main',
+                    url: 'https://github.com/faiz732/html_demo.git'
+            }
+        }
+        stage('Build Docker Image') {
+            steps {
+                bat 'docker build -t ${DOCKER_IMAGE} .'
+            }
+        }
+        stage('Push Image') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'dockerhub', 
+                    usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                    bat 'docker login -u %USER% -p %PASS%'
+                    bat 'docker push ${DOCKER_IMAGE}'
+                }
+            }
+        }
+        stage('Deploy to Kubernetes') {
+            steps {
+                withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+                    bat 'set KUBECONFIG=%KUBECONFIG%'
+                    bat 'kubectl apply -f deployment.yaml --validate=false'
+                }
+            }
+        }
     }
 }
